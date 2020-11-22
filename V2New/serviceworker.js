1c0766545aab58dc007673a3612c8c88895dab88
Kavinkumarsimple <74304337+Kavinkumarsimple@users.noreply.github.com>
2020-11-22 19:09:45 +0530

    Create serviceworker.js

---
+++ V2New/serviceworker.js
@@ -0,0 +1,28 @@
+//set up cache name and files to add to it
+const CACHE_NAME = 'TeaHouse_Cache';
+const CACHE_URLS =  [
+                     'index.html',
+                    'styles/style.css',
+                    'styles/style2.css',
+                     
+                     'scripts/main.js',
+                      'scripts/navcontrols.js'
+                     ];
+
+
+//set up cache and files to add to it
+//...
+
+//add all URLs to cache when installed
+self.addEventListener("install", function(event){
+    console.log("Service worker installed");
+    event.waitUntil(
+        //create and open cache
+        caches.open(CACHE_NAME)
+            .then(function(cache){
+                console.log("Cache opened");
+                //add all URLs to cache
+                return cache.addAll(CACHE_URLS);
+        })
+    );
+});
